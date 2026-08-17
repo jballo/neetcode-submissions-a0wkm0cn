52fc23ae3714f0e79d153583501d784f86367e68
Jonathan Ballona Sanchez <jballonasanchez@gmail.com>
2026-08-17 00:14:28 -0700

    Add: path-with-maximum-probability - submission-8

---
+++ Data Structures & Algorithms/path-with-maximum-probability/submission-8.py
@@ -0,0 +1,42 @@
+import heapq
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        highest = [-1] * n
+
+        graph = {}
+        for i in range(n + 1):
+            graph[i] = []
+
+
+        for i in range(len(edges)):
+            edge = edges[i]
+            a, b = edge[0], edge[1]
+            if a not in graph:
+                graph[a] = []
+            if b not in graph:
+                graph[b] = []
+            
+            graph[a].append((b, succProb[i]))
+            graph[b].append((a, succProb[i]))
+
+        
+
+        maxHeap = [(-1, start_node)]
+
+        while maxHeap:
+            p, n = heapq.heappop(maxHeap)
+            print("p: ", p, " n: ", n)
+            if n == end_node:
+                return -p
+            if highest[int(n)] != -1:
+                continue
+            
+            highest[n] = -1 * p
+
+            for n, np in graph[n]:
+                if highest[n] != -1:
+                    continue
+                heapq.heappush(maxHeap, (p * np, n))
+
+        
+        return 0 if highest[end_node] == -1 else highest[end_node]
