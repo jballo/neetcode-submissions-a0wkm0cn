e99ee725fb08e75b594d65e849d6ef5ac5fc279a
Jonathan Ballona Sanchez <jballonasanchez@gmail.com>
2026-08-22 23:09:49 -0700

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.py
@@ -0,0 +1,37 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = {}
+        for i in range(numCourses):
+            graph[i] = []
+
+        for src, dst in prerequisites:
+            graph[src].append(dst)
+
+        
+
+        order = []
+        visited = set()
+        path = set()
+
+        def dfs(i, o , g, v, p):
+            if i in p:
+                return False
+            if i in v:
+                return True
+
+            v.add(i)
+            p.add(i)
+            for neigh in g[i]:
+                if not dfs(neigh, o, g, v, p):
+                    return False
+
+            o.append(i)
+            p.remove(i)
+            return True
+        
+
+        for i in range(numCourses):
+            if not dfs(i, order, graph, visited, path):
+                return []
+
+        return order
